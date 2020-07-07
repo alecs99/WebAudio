b5734bf5a2a3b2abc77df07c28679b77733253b9
alecs99 <ciobanu.alecsandru@gmail.com>
2020-07-07 12:39:55 +0300

    Adaugare functionalitati Jump si salt

--- src/main/webapp/audio.js
+++ src/main/webapp/audio.js
@@ -5,17 +5,24 @@ const audioElement = document.querySelector('audio');
 const track = audioContext.createMediaElementSource(audioElement);
 
 
+
 const playButton = document.getElementById('start');
 const resumeButton = document.getElementById('resume');
+const rewindButton = document.getElementById('rewind');
+const forwardButton = document.getElementById('fForward');
 const stopButton = document.getElementById('stop');
 const playbackControl = document.getElementById('playback');
 const playbackValue = document.getElementById('playback-value');
 
 playButton.onclick = function(){
     if(resumeButton.dataset.playing  === 'false'){
+        let secs = document.getElementById("jump").value;
+        document.getElementById("jump"). disabled = true;
+        audioElement.currentTime = secs;
         audioElement.play();
         this.setAttribute('disabled', 'disabled');
         resumeButton.dataset.playing = 'true';
+        requestAnimationFrame(drawCanvas);
     }
 }
 
@@ -25,6 +32,7 @@ stopButton.onclick = function () {
         audioElement.pause();
         playButton.removeAttribute('disabled')
         playButton.setAttribute('enabled', 'enabled');
+        document.getElementById("jump"). disabled = false;
         resumeButton.dataset.playing = 'false';
     }
 }
@@ -49,6 +57,7 @@ audioElement.addEventListener('ended', () => {
     resumeButton.dataset.playing = 'false';
     playButton.removeAttribute('disabled')
     playButton.setAttribute('enabled', 'enabled');
+    document.getElementById("jump"). disabled = false;
 }, false);
 
 const gainNode = audioContext.createGain();
@@ -73,11 +82,25 @@ playbackControl.oninput = function(){
     playbackValue.innerHTML = playbackControl.value;
 }
 document.addEventListener("keydown", function (event) {
+    let secs = document.getElementById("jumping").value;
     if(event.which === 39){
-        audioElement.currentTime = audioElement.currentTime + 30;
+        audioElement.currentTime = audioElement.currentTime + parseFloat(secs);
     } else if (event.which === 37){
-        audioElement.currentTime = audioElement.currentTime - 30;
+        audioElement.currentTime = audioElement.currentTime - parseFloat(secs);
     }
 
 })
+rewindButton.onclick = function(){
+    var secs = document.getElementById("jumping").value;
+    console.log(secs);
+
+    audioElement.currentTime = audioElement.currentTime - parseFloat(secs);
+}
+forwardButton.onclick = function(){
+    var secs = document.getElementById("jumping").value;
+    console.log(audioElement.currentTime);
+    audioElement.currentTime = audioElement.currentTime + parseFloat(secs);
+    console.log(audioElement.currentTime);
+}
+
 track.connect(gainNode).connect(panner).connect(audioContext.destination);
--- src/main/webapp/listen.jsp
+++ src/main/webapp/listen.jsp
@@ -15,13 +15,18 @@
 <body>
 <h5>Asculta aici:</h5>
 <audio src="got.mp3" crossOrigin = "anonymous"></audio>
+
 <button id="start">Play</button>
+Jump to:<input type="text" id="jump" name="jump" placeholder="0">
 <button data-playing="false" id="resume" role="switch" aria-checked="false">Pause/Resume</button>
 <button id="stop">Stop</button>
 <input type="range" id="volume" min="0" max="1" value="0.5" step="0.01" data-action="volume">
 <input type="range" id="panner" min="-7" max="7" value="0" step="0.01" data-action="panner">
 <input type="range" id="playback" min="0.5" max="4" value="1" step="0.5">
 <span id="playback-value">1.0</span>
-<script src="audio.js"></script>
+Rewind/Fast forward: <input type="text" id="jumping" name="jumping" placeholder="0">
+<button id="rewind">Rewind</button>
+<button id="fForward">Fast Forward</button>
+<script src="audios.js"></script>
 </body>
 </html>
